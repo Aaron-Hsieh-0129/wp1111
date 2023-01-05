d1fc8dcaeb442dd180db68ca3226c193d210ca60
Aaron Hsieh <b08209006@ntu.edu.tw>
2023-01-05 16:02:18 +0800

    v1.0

---
+++ final/backend/src/index.js
@@ -0,0 +1,51 @@
+// import http from 'http'
+// import express from 'express';
+// import dotenv from 'dotenv-defaults'
+// import mongoose from 'mongoose'
+// import WebSocket from 'ws'
+// import mongo from './mongo'
+// import wsConnect from './wsConnect'
+// import { v4 as uuidv4 } from 'uuid'
+
+// mongo.connect();
+
+// const app = express()
+// const server = http.createServer(app)
+// const wss = new WebSocket.Server({ server })
+// const db = mongoose.connection
+
+// db.once('open', () => {
+//     console.log("MongoDB connected!")
+//     wss.on('connection', (ws) => { // ws: client-side websocket
+//         // wsConnect.initData(ws)
+//         // ws.id = uuidv4() // hash key
+//         // ws.box = '';
+//         // console.log(wss)
+//         ws.onmessage = wsConnect.onMessage(wss, ws);
+//     }); });
+
+// const PORT = process.env.PORT || 4001;
+// server.listen(PORT, () => {
+//     console.log(`Server is up on port ${PORT}.`)
+// })
+
+
+// import GraphQL server
+import server from './server'
+// import MongoDB connection
+import mongo from './mongo'
+mongo.connect();
+
+// deployment
+if (process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, "../frontend", "build")));
+    app.get("/*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+    });
+}
+
+const PORT = process.env.PORT || 4010;
+server.listen(PORT, () => {
+    console.log(`Server is up on port ${PORT}.`)
+})
---
+++ final/frontend/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import styled from 'styled-components';
+import Dashboard from './containers/Dashboard';
+import { useState } from 'react';
+import { useDisplay } from './containers/hooks/useDisplay';
+import ChatRoom from './containers/ChatRoom';
+import VideoRoom from './components/VideoRoom';
+
+const Wrapper = styled.div `
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 1000px;
+  margin: auto;
+`;
+
+function App() {
+    /* global google */
+    const {display, setDisplay, signedIn} = useDisplay();
+
+    const [user, setUser] = useState({});
+
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/chatroom/:id" element={<ChatRoom />} />
+                <Route path="/chatroom/:id/:roomID" element={<VideoRoom />} />
+            </Routes>
+        </Router> 
+    );
+}
+
+export default App;
