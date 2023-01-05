425cc08efa7f5fa8a0a1545c7351e70a159e9aa4
Aaron Hsieh <b08209006@ntu.edu.tw>
2023-01-05 19:50:27 +0800

    v1.47

--- final/frontend/src/index.js
+++ final/frontend/src/index.js
@@ -28,7 +28,7 @@ const httpLink = new HttpLink({
 
 // Create a WebSocket link:
 const wsLink = new GraphQLWsLink(createClient({
-    url: 'ws://localhost:4010/graphql',
+    url: 'ws://localhost:4010/subscriptions',
     options: { reconnect: true },
 }));
 
